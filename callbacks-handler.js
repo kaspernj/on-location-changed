5bff1f38c8a08e5d70f038d4cd842b4111e6babe
kaspernj <k@spernj.org>
2021-11-15 10:29:27 +0000

    Refactored to more object oriented and fixed multiple instances issue

---
+++ callbacks-handler.js
@@ -0,0 +1,62 @@
+import {digg} from "diggerize"
+import {OnLocationChangedCallback} from "./on-location-changed-callback"
+
+class CallbacksHandler {
+  constructor() {
+    this.callbacks = {}
+    this.count = 0
+    this.currentLocationHref = location.href
+  }
+
+  callCallbacks = () => {
+    for (const count in this.callbacks) {
+      this.callbacks[count].callCallback()
+    }
+  }
+
+  connectMutationObserver() {
+    // Solution recommended various places on the internet is to observe for changed and then check if the location has changed.
+    new MutationObserver(digg(this, "onLocationMightHaveChanged")).observe(document, {subtree: true, childList: true})
+  }
+
+  connectReactRouterHistory(history) {
+    // A React Router history can be registered globally (must be imported before this file).
+    history.listen(digg(this, "onLocationMightHaveChanged"))
+  }
+
+  connectWindowEvents() {
+    // If the hash has changed then maybe the entire location has? Trying to catch location change as early as possible.
+    window.addEventListener("hashchange", digg(this, "onLocationMightHaveChanged"))
+
+    // 'popstate' is only called doing certain actions (React Router won't trigger this for example).
+    window.addEventListener("popstate", digg(this, "onLocationMightHaveChanged"))
+  }
+
+  onLocationChanged = (givenCallback) => {
+    this.count += 1
+
+    const callback = new OnLocationChangedCallback(this, this.count, givenCallback)
+
+    this.callbacks[this.count] = callback
+
+    return callback
+  }
+
+  onLocationMightHaveChanged = () => {
+    if (location.href != this.currentLocationHref) {
+      this.currentLocationHref = location.href
+
+      this.callCallbacks()
+    }
+  }
+}
+
+// Prevent anything from spawning multiple instances (which happened!)
+if (!global.onLocationChangedCallbacksHandler) {
+  global.onLocationChangedCallbacksHandler = new CallbacksHandler()
+}
+
+const callbacksHandler = global.onLocationChangedCallbacksHandler
+
+// Export the single handler that is supposed to exist
+export {callbacksHandler}
