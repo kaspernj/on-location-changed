11817f5285ba88f9cb5168284678e109215eadb3
kaspernj <k@spernj.org>
2024-03-18 10:43:32 +0000

    Use memo to not re-render

--- src/with-path.jsx
+++ src/with-path.jsx
@@ -1,11 +1,12 @@
+import {memo} from "react"
 import usePath from "./use-path.js"
 
-const withPath = (WrappedComponent) => (props) => {
+const withPath = (WrappedComponent) => memo((props) => {
   const path = usePath()
 
   return (
     <WrappedComponent path={path} {...props} />
   )
-}
+})
 
 export default withPath
