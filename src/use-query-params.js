62943f4c5179db04f25cfce45de5658e6d9234e5
kaspernj <k@spernj.org>
2024-03-21 12:11:18 +0000

    useQueryParams improvements

--- src/use-query-params.js
+++ src/use-query-params.js
@@ -2,10 +2,13 @@ import {useCallback, useEffect, useState} from "react"
 import onLocationChanged from "./on-location-changed.js"
 import qs from "qs"
 
+const params = () => qs.parse(globalThis.location.search.substr(1)) || {}
+
 const useQueryParams = () => {
-  const params = useCallback(() => qs.parse(globalThis.location.search.substr(1)) || {}, [])
   const [queryParams, setQueryParams] = useState(params())
   const updateQueryParams = useCallback(() => setQueryParams(params()), [])
+
+  console.error(`DEBUG: useQueryParams - ${JSON.stringify(queryParams)}`)
 
   useEffect(() => {
     const onLocationChangedEvent = onLocationChanged(updateQueryParams)
@@ -13,7 +16,7 @@ const useQueryParams = () => {
     return () => {
       onLocationChangedEvent.disconnect()
     }
-  })
+  }, [])
 
   return queryParams
 }
