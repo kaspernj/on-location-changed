5399368a2f33ac1cf05a899897acf7b817e0a901
kaspernj <kasper@diestoeckels.de>
2026-05-19 12:17:28 +0000

    Expose query param update helpers

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import onSelectedQueryParamsChanged from "./on-selected-query-params-changed.js"
 import onLocationChanged from "./on-location-changed.js"
 import useLocationPath from "./use-location-path.js"
 import useSelectedQueryParams from "./use-selected-query-params.js"
+import useCurrentQueryParams from "./use-current-query-params.js"
+import {
+  locationPathWithQueryParams,
+  pathForQueryParamUpdates,
+  pushQueryParamUpdates
+} from "./query-param-updates.js"
 
 export {
   currentLocationPath,
   currentQueryParams,
+  locationPathWithQueryParams,
   onLocationChanged,
   onSelectedQueryParamsChanged,
+  pathForQueryParamUpdates,
+  pushQueryParamUpdates,
+  useCurrentQueryParams,
   useLocationPath,
   useSelectedQueryParams
 }
--- src/with-custom-path.jsx
+++ src/with-custom-path.jsx
@@ -1,6 +1,6 @@
-import React, {memo, useLayoutEffect, useMemo} from "react"
-import {LocationContext, LocationStoreContext, QueryParamsContext} from "./location-context"
-import {LocationStore} from "./location-store.js"
+import React, {memo, useEffect, useRef} from "react"
+import {LocationContext, LocationStoreContext, QueryParamsContext, defaultLocationStore} from "./location-context"
+import {queryParamsKey} from "./query-param-selector.js"
 
 const withCustomPath = memo(({children, path, queryParams, ...restProps}) => {
   const restPropsKeys = Object.keys(restProps)
@@ -9,15 +9,31 @@ const withCustomPath = memo(({children, path, queryParams, ...restProps}) => {
     throw new Error(`Unhandled props given: ${restPropsKeys.join(", ")}`)
   }
 
-  const locationStore = useMemo(() => new LocationStore(), [])
+  const typedPath = path || "/"
+  const typedQueryParams = queryParams || {}
+  const publishedSnapshotKeyRef = useRef("")
+  const snapshotKey = `${typedPath}\n${queryParamsKey(typedQueryParams)}`
 
-  locationStore.replaceSnapshotSilently({path, queryParams})
-  useLayoutEffect(locationStore.flushPendingNotification, [locationStore, path, queryParams])
+  defaultLocationStore.replaceSnapshotSilently({
+    path: typedPath,
+    queryParams: typedQueryParams
+  })
+
+  useEffect(() => {
+    if (publishedSnapshotKeyRef.current === snapshotKey) return
+
+    publishedSnapshotKeyRef.current = snapshotKey
+    queueMicrotask(() => {
+      if (publishedSnapshotKeyRef.current === snapshotKey) {
+        defaultLocationStore.flushPendingNotification()
+      }
+    })
+  }, [snapshotKey])
 
   return (
-    <LocationStoreContext.Provider value={locationStore}>
-      <LocationContext.Provider value={path}>
-        <QueryParamsContext.Provider value={queryParams}>
+    <LocationStoreContext.Provider value={defaultLocationStore}>
+      <LocationContext.Provider value={typedPath}>
+        <QueryParamsContext.Provider value={typedQueryParams}>
           {children}
         </QueryParamsContext.Provider>
       </LocationContext.Provider>
